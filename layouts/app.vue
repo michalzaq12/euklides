5ed550b785516205b6f120e49ae190a7d025317c
Michał Zarach <michal.m.zarach@gmail.com>
2019-04-08 11:05:34 +0200

    Update default layout

---
+++ layouts/app.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-app>
+        <navigation ref="nav"/>
+        <v-toolbar fixed app>
+            <v-toolbar-side-icon @click.stop="$refs.nav.toggle()"></v-toolbar-side-icon>
+            <v-toolbar-title>Euklides</v-toolbar-title>
+        </v-toolbar>
+        <v-content>
+            <nuxt />
+        </v-content>
+        <v-footer app>
+            <span class="white--text">&copy; 2019</span>
+        </v-footer>
+    </v-app>
+</template>
+
+<script>
+    import Navigation from '~/components/Navigation';
+    export default {
+        components: {Navigation}
+    }
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,5 @@
 <template>
     <v-app>
-        <navigation ref="nav"/>
-        <v-toolbar color="teal" dark fixed app>
-            <v-toolbar-side-icon @click.stop="$refs.nav.toggle()"></v-toolbar-side-icon>
-            <v-toolbar-title>Euklides</v-toolbar-title>
-        </v-toolbar>
-        <v-content>
-            <nuxt />
-        </v-content>
-        <v-footer color="teal" app>
-            <span class="white--text">&copy; 2019</span>
-        </v-footer>
+        <nuxt />
     </v-app>
 </template>
-
-<script>
-    import Navigation from '~/components/Navigation';
-    export default {
-        components: {Navigation}
-    }
-</script>
