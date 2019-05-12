da5e3ef7200a3c576a114c783d1d199116dbb65b
Michał Zarach <michal.m.zarach@gmail.com>
2019-05-12 17:36:57 +0200

    Resettable && TwoWayEnum

--- decorators/index.ts
+++ decorators/index.ts
@@ -1,1 +1,52 @@
+
 export * from 'nuxt-property-decorator'
+
+import * as cloneDeep from 'lodash/cloneDeep';
+
+
+
+
+interface IReset {
+    reset() : void;
+}
+
+
+export function Resettable<T>(value : T) : T & IReset{
+    const defaultValue = cloneDeep(value);
+    const returnValue = cloneDeep(value);
+
+    Object.defineProperty(returnValue, 'reset', {
+            enumerable: false,
+            writable: false,
+            value: function () {
+                for(const prop in returnValue){
+                    if(returnValue.hasOwnProperty(prop)){
+                        returnValue[prop] = cloneDeep(defaultValue[prop]);
+                    }
+                }
+            }
+        });
+
+    //@ts-ignore
+    return returnValue;
+}
+
+export class TwoWayEnum {
+
+    private keys = {};
+    private values = [];
+
+    constructor(elements) {
+        for(let i=0; i < elements.length; i++){
+            this.values.push(elements[i]);
+            this.keys[elements[i]] = i;
+        }
+    }
+
+    get(value: any) {
+        if(this.values[value] !== undefined) return this.values[value];
+        if(this.keys[value] !== undefined) return this.keys[value];
+        return undefined;
+    }
+
+}
