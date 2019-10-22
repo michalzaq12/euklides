680af2178000ea7f9c954a3a0254c86211854672
Michał Zarach <michal.m.zarach@gmail.com>
2019-10-22 16:57:49 +0200

    student's component

---
+++ pages/panel/student.component.vue
@@ -0,0 +1,147 @@
+<template>
+    <v-container class="panel__container" fluid fill-height>
+        <loader :active="isLoading" dark/>
+
+        <section class="welcome">
+            <v-card  height="160" color="primary" dark>
+                <div class="results__card">
+                    <div class="icon__placeholder">
+                        <div>
+                            <v-icon size="80">star</v-icon>
+                        </div>
+                    </div>
+                    <div class="results">
+                        <div>
+                            <div class="font-weight-thin text-xs-center">TYLE ZADAŃ ROZWIĄZAŁEŚ</div>
+                            <div class="font-weight-thin text-xs-center">W TYM TYGODNIU</div>
+                        </div>
+                        <div>
+                            <span class="display-3">4</span>
+                        </div>
+                    </div>
+                </div>
+            </v-card>
+
+            <v-card class="mt-5">
+                <v-toolbar class="pt-3" flat color="white">
+                    <v-icon color="grey darken-1">new_releases</v-icon>
+                    <v-toolbar-title class="grey--text text--darken-1">Nowe zadania</v-toolbar-title>
+                </v-toolbar>
+                <div>
+                    <div class="subheading grey--text text--darken-1" style="margin-left: 70px">Masz <span class="text--primary font-weight-bold">1</span> nowe zadanie domowe</div>
+                    <div class="d-flex pa-3 mt-3">
+                        <div style="width: 50%">
+                            <img src="~/assets/img/student/kid3.png" style="max-width: 100%; max-height: 100%"/>
+                        </div>
+                        <div style="align-self: center">
+                            <v-btn color="primary" large>ROZWIĄŻ</v-btn>
+                        </div>
+                    </div>
+                </div>
+            </v-card>
+        </section>
+
+        <section class="timeline">
+            <v-card height="auto">
+                <v-toolbar class="mb-2 py-3" flat>
+                    <v-icon color="grey darken-1">calendar_today</v-icon>
+                    <v-toolbar-title class="grey--text text--darken-1">Ostatnie zdarzenia</v-toolbar-title>
+                </v-toolbar>
+                <timeline class="ma-4"></timeline>
+            </v-card>
+        </section>
+
+    </v-container>
+</template>
+
+
+
+<script lang="ts">
+  import { Component, Vue } from "~/decorators";
+  import {UserStore} from "~/store/user";
+  import Timeline from '~/components/panel/Timeline.vue';
+
+  @Component({
+    layout: 'app',
+    head: {title: 'Panel'},
+    components: {Timeline}
+  })
+  export default class extends Vue {
+    isLoading = true;
+    user = UserStore.CreateProxy( this.$store, UserStore );
+
+    mounted(){
+      this.isLoading = true;
+      this.user.fetch().finally(() => {
+        this.isLoading = false;
+      })
+    }
+  }
+</script>
+
+
+<style lang="scss" scoped>
+    .panel__container {
+        display: flex;
+        width: 100%;
+        height: calc(100vh - 80px);
+        justify-content: space-between;
+        flex-wrap: wrap;
+
+        background: url("~assets/img/student/background.jpg") !important;
+        background-size: cover !important;
+        background-position: center center;
+    }
+
+    $xs-breakpoint: 600px;
+    $sm-breakpoint: 960px;
+
+    section.welcome{
+        width: 20%;
+        min-width: 400px;
+        margin-left: 100px;
+        @media screen and (max-width: $sm-breakpoint) {
+            margin-left: 0;
+            width: 100%;
+        }
+        display: flex;
+        flex-direction: column;
+
+        & .results__card {
+            width: 100%;
+            height: 100%;
+            display: flex;
+
+            & .icon__placeholder{
+                background-color: rgba(60, 60, 60, 0.5);
+                width: 30%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+            }
+
+            & .results {
+                width: 70%;
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                justify-content: center;
+            }
+        }
+    }
+
+
+    section.timeline {
+        width: 20%;
+        min-width: 400px;
+        margin-right: 100px;
+        @media screen and (max-width: $sm-breakpoint) {
+            width: 100%;
+            margin-right: 0;
+        }
+    }
+
+
+
+
+</style>
--- pages/panel/teacher.component.vue
+++ pages/panel/teacher.component.vue
@@ -21,7 +21,6 @@
                         </div>
                     </div>
                 </div>
-<!--                <div class="headline text-xs-center white&#45;&#45;text">Witaj {{user.firstName}}</div>-->
             </v-card>
             <v-card class="timeline" height="auto">
                 <v-toolbar class="mb-2 py-3" flat>
@@ -32,72 +31,72 @@
             </v-card>
         </section>
 
+        <section class="user__classes">
+            <v-card>
+                <v-toolbar dark color="primary" class="mb-2 py-3">
+                    <v-icon>book</v-icon>
+                    <v-toolbar-title>Lista Twoich klas</v-toolbar-title>
+                </v-toolbar>
 
-        <v-card class="user__classes">
-            <v-toolbar dark color="primary" class="mb-2 py-3">
-                <v-icon>book</v-icon>
-                <v-toolbar-title>Lista Twoich klas</v-toolbar-title>
-            </v-toolbar>
-
-            <v-expansion-panel class="pa-2" style="box-shadow: none">
-            <v-data-iterator
-                :items="classes"
-                :rows-per-page-items="rowsPerPageItems"
-                :pagination.sync="pagination"
-                style="width: 100%"
-                content-class="mb-5"
-            >
-                <template v-slot:item="props">
-                    <v-expansion-panel-content>
-                        <template #header>
-                            <div class="title" style="min-width: 100px">{{props.item.name}}</div>
-                            <div class="subheading">{{props.item.description}}</div>
-                        </template>
-                        <v-list>
-
-                            <template v-for="(student, index) in props.item.students">
-
-                                <v-divider></v-divider>
-
-                                <v-list-tile avatar @click="">
-                                    <v-list-tile-avatar color="blue-grey darken-1">
-                                        <v-icon dark>account_circle</v-icon>
-                                    </v-list-tile-avatar>
-
-                                    <v-list-tile-content>
-                                        <v-list-tile-title>{{student}}</v-list-tile-title>
-                                    </v-list-tile-content>
-
-                                    <v-list-tile-action>
-                                        <v-flex>
-                                            <v-btn icon ripple>
-                                                <v-icon color="grey lighten-1">edit</v-icon>
-                                            </v-btn>
-                                            <v-tooltip right>
-                                                <template #activator="{ on }">
-                                                    <v-btn v-on="on" icon large class="ml-5" color="primary" @click.stop="$refs.exSource.open()">
-                                                        <v-icon>add</v-icon>
-                                                    </v-btn>
-                                                </template>
-                                                <span>Zadaj zadanie</span>
-                                            </v-tooltip>
-                                        </v-flex>
-                                    </v-list-tile-action>
-
-                                </v-list-tile>
+                <v-expansion-panel class="pa-2" style="box-shadow: none">
+                <v-data-iterator
+                    :items="classes"
+                    :rows-per-page-items="rowsPerPageItems"
+                    :pagination.sync="pagination"
+                    style="width: 100%"
+                    content-class="mb-5"
+                >
+                    <template v-slot:item="props">
+                        <v-expansion-panel-content>
+                            <template #header>
+                                <div class="title" style="min-width: 100px">{{props.item.name}}</div>
+                                <div class="subheading">{{props.item.description}}</div>
                             </template>
-                        </v-list>
-                    </v-expansion-panel-content>
-                </template>
-            </v-data-iterator>
-            </v-expansion-panel>
+                            <v-list>
 
-        </v-card>
+                                <template v-for="(student, index) in props.item.students">
 
+                                    <v-divider></v-divider>
 
+                                    <v-list-tile avatar @click="">
+                                        <v-list-tile-avatar color="blue-grey darken-1">
+                                            <v-icon dark>account_circle</v-icon>
+                                        </v-list-tile-avatar>
 
-        <ex-soruce ref="exSource" @fromDatabase="$refs.exChooser.open()"></ex-soruce>
-        <ex-chooser ref="exChooser"></ex-chooser>
+                                        <v-list-tile-content>
+                                            <v-list-tile-title>{{student}}</v-list-tile-title>
+                                        </v-list-tile-content>
+
+                                        <v-list-tile-action>
+                                            <v-flex>
+                                                <v-btn icon ripple>
+                                                    <v-icon color="grey lighten-1">edit</v-icon>
+                                                </v-btn>
+                                                <v-tooltip right>
+                                                    <template #activator="{ on }">
+                                                        <v-btn v-on="on" icon large class="ml-5" color="primary" @click.stop="$refs.exSource.open()">
+                                                            <v-icon>add</v-icon>
+                                                        </v-btn>
+                                                    </template>
+                                                    <span>Zadaj zadanie</span>
+                                                </v-tooltip>
+                                            </v-flex>
+                                        </v-list-tile-action>
+
+                                    </v-list-tile>
+                                </template>
+                            </v-list>
+                        </v-expansion-panel-content>
+                    </template>
+                </v-data-iterator>
+                </v-expansion-panel>
+
+            </v-card>
+
+            <ex-soruce ref="exSource" @fromDatabase="$refs.exChooser.open()"></ex-soruce>
+            <ex-chooser ref="exChooser"></ex-chooser>
+        </section>
+
     </v-container>
 </template>
 
@@ -165,15 +164,15 @@
     .panel__container {
         display: flex;
         width: 100%;
-        height: 100vh;
+        height: calc(100vh - 80px);
         justify-content: space-around;
         flex-wrap: wrap;
 
         background: url("~assets/img/teacher/background.jpg") !important;
         background-size: cover !important;
         background-repeat: no-repeat;
-        background-position: right top;
-        background-attachment: fixed !important;
+        background-position: center center;
+        //background-attachment: fixed !important;
 
     }
 
@@ -183,7 +182,9 @@
     .welcome{
         width: 20%;
         min-width: 400px;
+        margin-left: 100px;
         @media screen and (max-width: $sm-breakpoint) {
+            margin-left: 0;
             width: 80%;
         }
         display: flex;
@@ -221,7 +222,9 @@
 
     .user__classes {
         width: 50%;
+        margin-right: 100px;
         @media screen and (max-width: $sm-breakpoint) {
+            margin-right: 0;
             width: 95%;
         }
         align-self: flex-start;
