3c68f09478b41f27a6a4c4059233a7582ac0040b
Michał Zarach <michal.m.zarach@gmail.com>
2019-12-18 09:44:29 +0100

    fix navGuard

--- middleware/navigationGuards.ts
+++ middleware/navigationGuards.ts
@@ -8,6 +8,7 @@ export default ({store, redirect, route}) => {
     if(process.server) return;
 
     const authStore = AuthStore.CreateProxy( store, AuthStore );
+    authStore.initStore();
 
     if(!unprotected.includes(route.path) && !authStore.isRefreshTokenValid) {
         redirect({
