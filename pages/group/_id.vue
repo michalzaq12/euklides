00c934095ba7fff8b4a22df08db2957130724c41
Michał Zarach <michal.m.zarach@gmail.com>
2020-01-07 18:01:47 +0100

    fetching students answer

--- pages/group/_id.vue
+++ pages/group/_id.vue
@@ -41,11 +41,11 @@
                         <v-data-table
                             :headers="headers"
                             :items="exercises"
-                            item-key="id"
+                            item-key="guid"
                             :rows-per-page-items="[10,20,50]"
                         >
                             <template #items="props">
-                                <tr>
+                                <tr @click="props.expanded = !props.expanded; fetchAnswers(props.item.homeworkId, props.item.id)">
                                     <td>{{props.item.groupName}}</td>
                                     <th class="text-xs-center">
                                         <v-menu open-on-hover offset-x>
@@ -60,8 +60,24 @@
                                     <td>{{props.item.chapter || '-'}}</td>
                                     <td class="font-weight-bold">{{ props.item.name }}</td>
                                     <td class="text-xs-center">{{new Date(props.item.deadline).toLocaleString()}}</td>
-                                    <td><v-btn @click="fetchAnswers(props.item.homeworkId)">Sprawdź</v-btn></td>
+                                    <td>
+                                        <v-icon v-if="props.expanded">keyboard_arrow_up</v-icon>
+                                        <v-icon v-else>keyboard_arrow_down</v-icon>
+                                    </td>
                                 </tr>
+                            </template>
+                            <template #expand="props">
+                                <v-card flat>
+                                    <v-card-text>
+                                        <v-data-table :items="props.item.answers" :headers="headers" :loading="isLoadingAnswers">
+                                            <template v-slot:items="props2">
+                                                <td>{{ props2.item.firstName }}</td>
+                                                <td>{{props2.item.lastName}}</td>
+                                                <td><v-btn>Sprawdź</v-btn></td>
+                                            </template>
+                                        </v-data-table>
+                                    </v-card-text>
+                                </v-card>
                             </template>
                         </v-data-table>
                     </v-card-text>
@@ -90,6 +106,7 @@
   })
   export default class extends Vue {
     isLoading = true;
+    isLoadingAnswers = true;
     user = UserStore.CreateProxy( this.$store, UserStore );
 
     rowsPerPageItems = [5, 10, 20];
@@ -125,9 +142,18 @@
         return this.$route.params.id;
     }
 
-    private async fetchAnswers(homeworkId){
+    private async fetchAnswers(homeworkId, exId){
+      this.isLoadingAnswers = true;
       const res = await this.$api.homeworks.$getHomeworkWithAnswersGroupedByStudent(homeworkId);
       console.log(res);
+      const students = res.studentsWithAnswersToHomework;
+      for(const student of students){
+        student.answer = student.exercisesToAnswers[exId];
+      }
+      console.log(students);
+      const ex = this.exercises.find(ex => ex.id === exId);
+      ex.answers = students;
+      this.isLoadingAnswers = false;
     }
 
     private async fetchData(){
@@ -142,6 +168,7 @@
       for(const homework of homeworkRes.items){
         for(const exercise of homework.exercises){
           this.exercises.push({
+            guid: homework.id + exercise.id,
             deadline: homework.deadline,
             homeworkId: homework.id,
             ...exercise,
