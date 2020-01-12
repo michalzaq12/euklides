f008e96f1ccadd8dc89ede441ad6ba9f31a8b9a7
Michał Zarach <michal.m.zarach@gmail.com>
2020-01-12 19:45:53 +0100

    homework page

--- pages/zadania-domowe.vue
+++ pages/zadania-domowe.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="ma-4">
         <v-data-table
             :headers="headers"
             :items="exercises"
@@ -10,6 +10,14 @@
         >
             <template #items="props">
                 <tr @click.stop="showExercise(props.item)">
+                    <td>
+                        <v-icon v-if="props.item.status" color="primary">radio_button_checked</v-icon>
+                        <v-icon v-else color="error">radio_button_unchecked</v-icon>
+                    </td>
+                    <td class="text-xs-center">
+                        <span v-if="props.item.status">{{props.item.userAnswer.commissionDateTime | date}}</span>
+                        <span v-else>-</span>
+                    </td>
                     <td>{{props.item.groupName}}</td>
                     <th class="text-xs-center">
                         <v-menu open-on-hover offset-x>
@@ -25,7 +33,11 @@
                     </th>
                     <td>{{props.item.chapter || '-'}}</td>
                     <td class="font-weight-bold">{{ props.item.name }}</td>
-                    <td class="text-xs-center">{{new Date(props.item.deadline).toLocaleString()}}</td>
+                    <td class="text-xs-center">{{props.item.deadline | date}}</td>
+                    <td>
+                        <v-btn v-if="!props.item.status" color="primary" flat>Prześlij odpowiedź</v-btn>
+                        <v-btn v-else color="primary" flat>Popraw odpowiedź</v-btn>
+                    </td>
                 </tr>
             </template>
         </v-data-table>
@@ -51,11 +63,50 @@
     user = UserStore.CreateProxy( this.$store, UserStore );
 
     exercises = [];
-    focusedExercise = null;
+    focusedExercise = {};
     showDialog = false;
 
     loading = false;
-    headers = [];
+    headers = [
+      {
+        text: 'Status',
+        value: 'status'
+      },
+      {
+        text: 'Data przesłania',
+        value: 'commissionDateTime',
+        align: 'center'
+      },
+      {
+        text: 'Grupa',
+        value: 'groupName',
+      },
+      {
+        text: 'Podgląd',
+        align: 'center',
+        sortable: false,
+        width: 50
+      },
+      {
+        text: 'Dział',
+        value: 'chapter',
+      },
+      {
+        text: 'Nazwa zadania',
+        value: 'name',
+      },
+      {
+        text: 'Termin oddania',
+        value: 'deadline',
+        align: 'center'
+      },
+      {
+        text: '',
+        align: 'center',
+        sortable: false,
+        width: 50
+      },
+    ];
 
     mounted(){
         this.fetchMyHomework();
@@ -77,8 +128,11 @@
         });
 
         for(const homework of homeworkRes.items){
+          const myAnswer = await this.$api.homeworks.$getHomeworkWithAnswersOfCurrentUser(homework.id);
           for(const exercise of homework.exercises){
             this.exercises.push({
+              status: myAnswer.exercisesToAnswers[exercise.id] !== null,
+              userAnswer: myAnswer.exercisesToAnswers[exercise.id],
               groupName: group.code,
               deadline: homework.deadline,
               homeworkId: homework.id,
@@ -88,17 +142,21 @@
         }
 
       }
+      const withoutAnswer = this.exercises.filter(el => el.status === false);
+      const withAnswer = this.exercises.filter(el => el.status === true);
+      this.exercises = [...withoutAnswer, ...withAnswer];
       this.loading = false;
-      console.log(this.exercises);
     }
 
 
     async sendAnswer(payload){
       this.showDialog = false;
       await this.$api.homeworks.commitAnswersToHomework({
+        //@ts-ignore
         homeworkId: this.focusedExercise.homeworkId,
         request: payload
       });
+      this.fetchMyHomework();
     }
 
   }
