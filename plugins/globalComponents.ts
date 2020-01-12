f31c92b4ca916bb24638e02249490b027573fb28
Michał Zarach <michal.m.zarach@gmail.com>
2020-01-12 18:05:15 +0100

    vue date filter

--- plugins/globalComponents.ts
+++ plugins/globalComponents.ts
@@ -1,4 +1,21 @@
 import Vue from 'vue'
-import Loader from '~/components/helpers/Loader.vue';
+import moment from "moment";
 
+
+// FILTERS
+
+Vue.filter('date', function (value) {
+    if (!value) return null;
+    return moment(value).format('DD/MM/YYYY HH:mm');
+});
+
+Vue.filter('dateonly', function (value) {
+    if (!value) return null;
+    return moment(value).format('DD / MM / YYYY');
+});
+
+
+// COMPONENTS
+
+import Loader from '~/components/helpers/Loader.vue';
 Vue.component('loader', Loader);
